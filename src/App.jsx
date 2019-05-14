cdccac9102f840ad6940e1663c9927b5954e3e72
jackw01 <jackw01@users.noreply.github.com>
2019-05-13 18:43:45 -0700

    get svg dom to work in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import Triangulator from 'triangulator2';
 
 class App extends Component {
   constructor(props) {
@@ -10,13 +11,45 @@ class App extends Component {
     this.state = {
 
     };
+
+    this.run();
+  }
+
+  run() {
+
+  }
+
+  generateSVG(element) {
+    const svgString = Triangulator.generate({
+      svgInput: element,
+      seed: 4,
+      width: 3840,
+      height: 2400,
+      gridMode: Triangulator.GridMode.Poisson,
+      gridOverride: false,
+      cellSize: 150,
+      cellRandomness: 0.2,
+      colorOverride: false,
+      color: Triangulator.ColorFunction.RadialFromBottom,
+      colorPalette: ['#e7a71d', '#dc433e', '#9e084b', '#41062f'],
+      colorRandomness: 0.15,
+      quantizeSteps: 0,
+      useGradient: true,
+      gradient: Triangulator.GradientFunction.Random,
+      gradientNegativeFactor: 0.03,
+      gradientPositiveFactor: 0.03,
+      strokeColor: false,
+      strokeWidth: 1,
+    });
+
+    console.log(svgString);
   }
 
   render() {
     return (
       <div className='main h-100'>
         <Container className='app h-100 d-flex flex-column' fluid>
-          aa
+          <svg ref={this.generateSVG} />
         </Container>
       </div>
     );
