ed6852fd18b4480a351e7489b6ef6be156f76d47
jackw01 <jackw01@users.noreply.github.com>
2019-06-12 13:03:53 -0700

    build

---
+++ build/precache-manifest.cdf675132b482ffe4a853499c1a336d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5678cc418a9bc197549eb783200cc708",
+    "url": "/triangulator2-app/build/index.html"
+  },
+  {
+    "revision": "230310cfa5f66fde2953",
+    "url": "/triangulator2-app/build/static/css/main.a82710ac.chunk.css"
+  },
+  {
+    "revision": "bd50586ea0d3ca26dcf6",
+    "url": "/triangulator2-app/build/static/js/2.c448cc7b.chunk.js"
+  },
+  {
+    "revision": "230310cfa5f66fde2953",
+    "url": "/triangulator2-app/build/static/js/main.1a38223f.chunk.js"
+  },
+  {
+    "revision": "ff9bd5580fde111d28d6",
+    "url": "/triangulator2-app/build/static/js/runtime~main.2e430073.js"
+  }
+]);
